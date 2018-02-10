3eb94b4a8ae29454849c8920c7f5e8e87c506767
CharlesZed <a44994966@163.com>
2018-02-10 08:56:16 +0000

    Finish user signup

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,8 +2,8 @@
   <footer class="footer">
     <small class="slogon">
       <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://laravel-china.org/courses">
-        刻意练习，每日精进
+      <a href="https://www.baidu.com">
+        生命不息，战斗不止
       </a>
     </small>
     <nav>
